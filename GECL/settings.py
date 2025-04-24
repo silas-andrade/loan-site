7e370183877ccf46dc1ab9da21f453f79dd37415
Silas <silasandrade.code@gmail.com>
2025-04-24 19:57:45 -0300

    Alteração no settings.py

--- GECL/settings.py
+++ GECL/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from pathlib import Path
+import os
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Acessando as variáveis de ambiente
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_0#bw_th49_4*mi$&%w@o)+qygfeylx009lr!6r465#%%n8u2'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
