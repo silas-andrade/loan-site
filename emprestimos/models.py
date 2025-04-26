c004635b2cdb86dac436ab28bfdcbad412be51d7
Silas <silasandrade.code@gmail.com>
2025-04-26 13:51:16 -0300

    fix: Alterei o modelo Emprestimo para a data de devolução fica vazia e fiz as migrações

--- emprestimos/models.py
+++ emprestimos/models.py
@@ -6,8 +6,12 @@ from moderator.models import *
 class Emprestimo(models.Model):
     aluno = models.ForeignKey(Aluno, on_delete=models.DO_NOTHING)
     material = models.ForeignKey(Material, on_delete=models.DO_NOTHING)
+    quantidade = models.PositiveIntegerField()
     data_prevista = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
-    data_devolvida = models.DateTimeField()
+    data_devolvida = models.DateTimeField(blank=True, null=True)
     devolvido = models.BooleanField(default=False)
     devolução_confirmada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.aluno} | {self.material} | {self.devolvido} | {self.devolução_confirmada}'
