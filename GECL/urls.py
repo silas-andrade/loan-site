ed593518bc2fe129cb48d080df31d3ab78f9e5ce
Silas <silasandrade.code@gmail.com>
2025-04-26 13:52:43 -0300

    feat: Adicionei mais rotas do App Moderator

--- GECL/urls.py
+++ GECL/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from usuarios.views import RegisterPage, LoginPage, LogoutUser
+from usuarios.views import RegisterPage, LoginPage, LogoutUser, DashboardAluno
 from core.views import HomePage
 from emprestimos.views import SolicitarEmprestimo, VerMeusEmprestimosPedidos, FazerDevolucao
-from moderator.views import DashboardAdmin, VerAlterarMateriais, AceitarDevolucao, AceitarPedido, RecusarPedido
+from moderator.views import DashboardAdmin, VerMateriais, AceitarDevolucao, AceitarPedido, RecusarPedido, RemoverMateriais, BloquearUsuarios, GerenciarAlunos
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
     
     # App Core
-    path('', HomePage, name='home'),
 
     # App usuarios
     path('registrar/', RegisterPage, name='cadastrar'),
@@ -20,14 +19,24 @@ urlpatterns = [
     # App Empréstimos
     path('solicitar/', SolicitarEmprestimo, name='solicitar'),
     path('meus-emprestimos/', VerMeusEmprestimosPedidos, name='meus-emprestimos'),
-    path('devolver/', FazerDevolucao, name='devolver'),
+    path('devolver/<str:pk>', FazerDevolucao, name='devolver'),
 
     # App Moderator
     path('dashboard/', DashboardAdmin, name='dashboard'),
-    path('ver-materiais/', VerAlterarMateriais, name='ver-materiais'),
-    path('aceitar-devolucao/<int:pk>', AceitarDevolucao, name='aceitar-devolucao'),
-    path('aceitar-pedido/<int:pk>', AceitarPedido, name='aceitar-pedido'),
-    path('recusar-pedido/<int:pk>', RecusarPedido, name='recusar-pedido'),
+    path('', DashboardAluno, name='dashboard-aluno'),
+
+    path('ver-materiais/', VerMateriais, name='ver-materiais'),
+    path('remover-materiais/<str:pk>', RemoverMateriais, name='remover-material'),
+    
+    path('aceitar-devolucao/<str:pk>', AceitarDevolucao, name='aceitar-devolucao'),
+    
+    path('aceitar-pedido/<str:pk>', AceitarPedido, name='aceitar-pedido'),
+    path('recusar-pedido/<str:pk>', RecusarPedido, name='recusar-pedido'),
+
+
+
+    path('gerenciar-alunos/', GerenciarAlunos, name='gerenciar-alunos'),
+    path('bloquear-usuarios/<str:pk>', BloquearUsuarios, name='bloquear-usuarios'),
 
 
 ]
