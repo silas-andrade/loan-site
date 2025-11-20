691a5fd32c368007b88df7e8de4689944b56afb4
Silas <silasandrade.code@gmail.com>
2025-11-20 12:52:00 -0300

    Refatorar configuração do banco de dados para usar urlparse e melhorar a segurança

--- core/settings.py
+++ core/settings.py
@@ -12,13 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-
 from dotenv import load_dotenv
 
-load_dotenv()
 
+from urllib.parse import urlparse, parse_qsl
 # Acessando as variáveis de ambiente
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
@@ -100,10 +101,19 @@ if DEBUG:
         }
     }
 else:
-    import dj_database_url
-    DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
-    }
+        tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': tmpPostgres.path.replace('/', ''),
+                'USER': tmpPostgres.username,
+                'PASSWORD': tmpPostgres.password,
+                'HOST': tmpPostgres.hostname,
+                'PORT': 5432,
+                'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
+            }
+        }
 
 
 
